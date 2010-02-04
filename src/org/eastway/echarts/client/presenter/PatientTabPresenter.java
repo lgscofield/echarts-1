0e859a8406bcfd40708c32ca346a4d27add02a69
Ian Hilt <ihilt@eastway.org>
2010-02-04 10:59:16 -0500

    Wire up the patient menu items
    This commit creates the structure and mechanism for selecting menu items
    and displaying the results based on which item was selected.  Also, it creates
    the structure for displaying the various corresponding panel items, e.g.
    IspPanel.
    
    Signed-off-by: Ian Hilt <ihilt@eastway.org>

--- src/org/eastway/echarts/client/presenter/PatientTabPresenter.java
+++ src/org/eastway/echarts/client/presenter/PatientTabPresenter.java
@@ -2,16 +2,39 @@ package org.eastway.echarts.client.presenter;
 
 import org.eastway.echarts.client.PatientServicesAsync;
 
+import com.google.gwt.event.logical.shared.HasSelectionHandlers;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
 import com.google.gwt.event.shared.HandlerManager;
+import com.google.gwt.user.client.ui.HasText;
+import com.google.gwt.user.client.ui.TreeItem;
 
 public class PatientTabPresenter extends EchartsPresenter<PatientTabPresenter.Display> {
 	public interface Display extends EchartsDisplay {
 		void setPatientId(String patientId);
 
 		String getPatientId();
+
+		HasSelectionHandlers<TreeItem> getPatientMenu();
+
+		void setDisplay(HasText selectedItem);
+
+		void initDisplayArea();
 	}
 
 	public PatientTabPresenter(Display display, HandlerManager eventBus, PatientServicesAsync patientSvc, String patientId) {
 		super(display, eventBus);
+		display.setPatientId(patientId);
+		display.initDisplayArea();
+		bind();
+	}
+
+	private void bind() {
+		display.getPatientMenu().addSelectionHandler(new SelectionHandler<TreeItem>() {
+			@Override
+			public void onSelection(SelectionEvent<TreeItem> event) {
+				display.setDisplay(event.getSelectedItem());
+			}
+		});
 	}
 }
--- src/org/eastway/echarts/client/view/PatientTabView.java
+++ src/org/eastway/echarts/client/view/PatientTabView.java
@@ -2,12 +2,19 @@ package org.eastway.echarts.client.view;
 
 import org.eastway.echarts.client.presenter.PatientTabPresenter;
 
+import com.google.gwt.event.logical.shared.HasSelectionHandlers;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HasText;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Tree;
+import com.google.gwt.user.client.ui.TreeItem;
 import com.google.gwt.user.client.ui.Widget;
+
+import org.eastway.echarts.client.IspPanel;
 
 public class PatientTabView extends Composite implements PatientTabPresenter.Display {
 	private static PatientTabViewUiBinder uiBinder = GWT.create(PatientTabViewUiBinder.class);
@@ -17,9 +24,23 @@ public class PatientTabView extends Composite implements PatientTabPresenter.Dis
 
 	private String patientId = null;
 	@UiField Tree patientMenu;
+	@UiField FlowPanel displayArea;
+
+
+	private TreeItem personal, demographics, progressNote, messages,
+			addMessage, treatmentPlan, serviceHistory;
+
+	private IspPanel ispPanel;
 
 	public PatientTabView() {
 		initWidget(uiBinder.createAndBindUi(this));
+		this.personal = patientMenu.addItem("Personal");
+		this.demographics = patientMenu.addItem("Demographics");
+		this.progressNote = patientMenu.addItem("Progress Note");
+		this.messages = patientMenu.addItem("Messages");
+		this.addMessage = messages.addItem("Add");
+		this.treatmentPlan = patientMenu.addItem("Treatment Plan");
+		this.serviceHistory = patientMenu.addItem("Service History");
 	}
 
 	@Override
@@ -35,5 +56,32 @@ public class PatientTabView extends Composite implements PatientTabPresenter.Dis
 	@Override
 	public String getPatientId() {
 		return patientId;
+	}
+
+	@Override
+	public HasSelectionHandlers<TreeItem> getPatientMenu() {
+		return patientMenu;
+	}
+
+	@Override
+	public void setDisplay(HasText selectedItem) {
+		if (selectedItem == personal) {
+		} else if (selectedItem == demographics) {
+		} else if (selectedItem == progressNote) {
+		} else if (selectedItem == messages) {
+		} else if (selectedItem == addMessage) {
+		} else if (selectedItem == treatmentPlan) {
+			displayArea.clear();
+			displayArea.add(ispPanel);
+		} else if (selectedItem == serviceHistory) {
+		} else {
+			displayArea.clear();
+			displayArea.add(new HTML("This menu item not yet available"));
+		}
+	}
+
+	@Override
+	public void initDisplayArea() {
+		this.ispPanel = new IspPanel(patientId);
 	}
 }
