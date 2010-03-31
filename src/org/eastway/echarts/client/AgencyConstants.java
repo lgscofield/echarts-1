baf9fe327d8324d049b4697afdd50973e96ada21
Ian Hilt <ihilt@eastway.org>
2010-03-31 15:13:59 -0400

    Made gmh provided/not provided services a String property
    Signed-off-by: Ian Hilt <ihilt@eastway.org>

--- src/org/eastway/echarts/client/AgencyConstants.java
+++ src/org/eastway/echarts/client/AgencyConstants.java
@@ -4,4 +4,6 @@ import com.google.gwt.i18n.client.Constants;
 
 public interface AgencyConstants extends Constants {
 	String agencyName();
+	String gmhServicesProvided();
+	String gmhServicesNotProvided();
 }
--- src/org/eastway/echarts/dashboard/client/GMHIntakeWizard.java
+++ src/org/eastway/echarts/dashboard/client/GMHIntakeWizard.java
@@ -12,7 +12,6 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.HTMLPanel;
