c10c9e646713b6467962246893c8c1c55bd867b9
Ian Hilt <ihilt@eastway.org>
2011-05-14 17:14:20 -0400

    Show description of some http status errors
    Signed-off-by: Ian Hilt <ihilt@eastway.org>

--- src/main/java/org/eastway/echartsrequest/client/EchartsAuthRequestTransport.java
+++ src/main/java/org/eastway/echartsrequest/client/EchartsAuthRequestTransport.java
@@ -27,8 +27,23 @@ public class EchartsAuthRequestTransport extends DefaultRequestTransport {
 						eventBus.fireEvent(new EchartsAuthenticationFailureEvent(loginUrl));
 						return;
 					}
+				} else if (response.getStatusCode() == Response.SC_INTERNAL_SERVER_ERROR) {
+					receiver.onTransportFailure(new ServerFailure("Server Failure: Internal Server Error"));
+					return;
+				} else if (response.getStatusCode() == Response.SC_NOT_IMPLEMENTED) {
+					receiver.onTransportFailure(new ServerFailure("Server Failure: Not Implemented"));
+					return;
+				} else if (response.getStatusCode() == Response.SC_BAD_GATEWAY) {
+					receiver.onTransportFailure(new ServerFailure("Server Failure: Bad Gateway"));
+					return;
+				} else if (response.getStatusCode() == Response.SC_SERVICE_UNAVAILABLE) {
+					receiver.onTransportFailure(new ServerFailure("Server Failure: Service Unavailable"));
+					return;
+				} else if (response.getStatusCode() == Response.SC_GATEWAY_TIMEOUT) {
+					receiver.onTransportFailure(new ServerFailure("Server Failure: Gateway Timeout"));
+					return;
 				} else if (response.getStatusCode() != Response.SC_OK) {
-					receiver.onTransportFailure(new ServerFailure("Server failure: " + response.getStatusCode(), null, null, true));
+					receiver.onTransportFailure(new ServerFailure("Server failure: " + response.getStatusCode()));
 					return;
 				}
 				superCallback.onResponseReceived(request, response);
